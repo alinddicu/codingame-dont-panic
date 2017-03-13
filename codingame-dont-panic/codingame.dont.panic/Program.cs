345e19ff30f494c920a9a89ffccf1a17f712524f
Alin Dicu <alinddicu@yahoo.com>
2017-03-13 11:43:22 +0100

    refactored CloneMaster.Decide into implementations ofTurnDecisionBase

--- codingame-dont-panic/codingame.dont.panic/Program.cs
+++ codingame-dont-panic/codingame.dont.panic/Program.cs
@@ -27,7 +27,7 @@
 		public void Run()
 		{
 			var driveParams = new DriveParams(_readLine);
-			var drive = new CloneMaster(driveParams);
+			var drive = new CloneMaster(driveParams, new TurnDecisionsFactory(driveParams));
 
 			// game loop
 			while (true)
@@ -57,68 +57,206 @@
 		EXIT
 	}
 
+	public abstract class TurnDecisionBase
+	{
+		protected TurnDecisionBase(DriveParams driveParams)
+		{
+			DriveParams = driveParams;
+		}
+
+		protected DriveParams DriveParams { get; }
+
+		public abstract bool CanDecide(TurnParams turnParams, int[] blockedClonesPerFloor);
+
+		public abstract TurnDecision Decide(TurnParams turnParams, int[] blockedClonesPerFloor);
+
+		protected Elevator GetCurrentFloorElevator(TurnParams turnParams)
+		{
+			return GetFloorElevator(turnParams, 0);
+		}
+
+		protected Elevator GetPreviousFloorElevator(TurnParams turnParams)
+		{
+			return GetFloorElevator(turnParams, 1); ;
+		}
+
+		private Elevator GetFloorElevator(TurnParams turnParams, int offSet)
+		{
+			return DriveParams.Elevators.FirstOrDefault(e => e.Floor == turnParams.CloneFloor - offSet);
+		}
+
+		protected TurnDecision IncrementBlockedClonesPerFloorAndBlock(TurnParams turnParams, int[] blockedClonesPerFloor)
+		{
+			blockedClonesPerFloor[turnParams.CloneFloor]++;
+			return TurnDecision.BLOCK;
+		}
+	}
+
+	public class BlockCloneBeforeColision : TurnDecisionBase
+	{
+		public BlockCloneBeforeColision(DriveParams driveParams) : base(driveParams)
+		{
+		}
+
+		public override bool CanDecide(TurnParams turnParams, int[] blockedClonesPerFloor)
+		{
+			return turnParams.IsLeftColision() || turnParams.IsRightColision(DriveParams.DriveWidth);
+		}
+
+		public override TurnDecision Decide(TurnParams turnParams, int[] blockedClonesPerFloor)
+		{
+			return IncrementBlockedClonesPerFloorAndBlock(turnParams, blockedClonesPerFloor);
+		}
+	}
+
+	public class BlockCloneIfGoingRightAndElevatorIsOnLeft : TurnDecisionBase
+	{
+		public BlockCloneIfGoingRightAndElevatorIsOnLeft(DriveParams driveParams) : base(driveParams)
+		{
+		}
+
+		public override bool CanDecide(TurnParams turnParams, int[] blockedClonesPerFloor)
+		{
+			var currentFloorElevator = GetCurrentFloorElevator(turnParams);
+			var previousFloorElevator = GetPreviousFloorElevator(turnParams);
+			return
+				turnParams.ClonePosition > currentFloorElevator?.Position
+				&& turnParams.Direction == Direction.RIGHT
+				&& turnParams.CloneFloor == currentFloorElevator?.Floor
+				&& previousFloorElevator?.Position + 1 == turnParams.ClonePosition
+				&& blockedClonesPerFloor[turnParams.CloneFloor] == 0;
+		}
+
+		public override TurnDecision Decide(TurnParams turnParams, int[] blockedClonesPerFloor)
+		{
+			return IncrementBlockedClonesPerFloorAndBlock(turnParams, blockedClonesPerFloor);
+		}
+	}
+
+	public class BlockCloneIfGoingRightAndExitIsOnLeft : TurnDecisionBase
+	{
+		public BlockCloneIfGoingRightAndExitIsOnLeft(DriveParams driveParams) : base(driveParams)
+		{
+		}
+
+		public override bool CanDecide(TurnParams turnParams, int[] blockedClonesPerFloor)
+		{
+			var previousFloorElevator = GetPreviousFloorElevator(turnParams);
+			return 
+				turnParams.ClonePosition > DriveParams.ExitPosition
+				&& turnParams.Direction == Direction.RIGHT
+				&& turnParams.CloneFloor == DriveParams.ExitFloor
+				&& previousFloorElevator?.Position + 1 == turnParams.ClonePosition
+				&& blockedClonesPerFloor[turnParams.CloneFloor] == 0;
+		}
+
+		public override TurnDecision Decide(TurnParams turnParams, int[] blockedClonesPerFloor)
+		{
+			return IncrementBlockedClonesPerFloorAndBlock(turnParams, blockedClonesPerFloor);
+		}
+	}
+
+	public class BlockCloneIfGoingLeftAndElevatorIsOnRight : TurnDecisionBase
+	{
+		public BlockCloneIfGoingLeftAndElevatorIsOnRight(DriveParams driveParams) : base(driveParams)
+		{
+		}
+
+		public override bool CanDecide(TurnParams turnParams, int[] blockedClonesPerFloor)
+		{
+			var currentFloorElevator = GetCurrentFloorElevator(turnParams);
+			var previousFloorElevator = GetPreviousFloorElevator(turnParams);
+			return
+				turnParams.ClonePosition < currentFloorElevator?.Position
+				&& turnParams.Direction == Direction.LEFT
+				&& turnParams.CloneFloor == currentFloorElevator?.Floor
+				&& previousFloorElevator?.Position - 1 == turnParams.ClonePosition
+				&& blockedClonesPerFloor[turnParams.CloneFloor] == 0;
+		}
+
+		public override TurnDecision Decide(TurnParams turnParams, int[] blockedClonesPerFloor)
+		{
+			return IncrementBlockedClonesPerFloorAndBlock(turnParams, blockedClonesPerFloor);
+		}
+	}
+
+	public class BlockCloneIfGoingLeftAndExitIsOnRight : TurnDecisionBase
+	{
+		public BlockCloneIfGoingLeftAndExitIsOnRight(DriveParams driveParams) : base(driveParams)
+		{
+		}
+
+		public override bool CanDecide(TurnParams turnParams, int[] blockedClonesPerFloor)
+		{
+			var previousFloorElevator = GetPreviousFloorElevator(turnParams);
+			return turnParams.ClonePosition < DriveParams.ExitPosition
+				&& turnParams.Direction == Direction.LEFT
+				&& turnParams.CloneFloor == DriveParams.ExitFloor
+				&& previousFloorElevator?.Position - 1 == turnParams.ClonePosition
+				&& blockedClonesPerFloor[turnParams.CloneFloor] == 0;
+		}
+
+		public override TurnDecision Decide(TurnParams turnParams, int[] blockedClonesPerFloor)
+		{
+			return IncrementBlockedClonesPerFloorAndBlock(turnParams, blockedClonesPerFloor);
+		}
+	}
+
+	public class Wait : TurnDecisionBase
+	{
+		public Wait(DriveParams driveParams) : base(driveParams)
+		{
+		}
+
+		public override bool CanDecide(TurnParams turnParams, int[] blockedClonesPerFloor)
+		{
+			return true;
+		}
+
+		public override TurnDecision Decide(TurnParams turnParams, int[] blockedClonesPerFloor)
+		{
+			return TurnDecision.WAIT;
+		}
+	}
+
+	public class TurnDecisionsFactory
+	{
+		private readonly DriveParams _driveParams;
+
+		public TurnDecisionsFactory(DriveParams driveParams)
+		{
+			_driveParams = driveParams;
+		}
+
+		public IEnumerable<TurnDecisionBase> Create()
+		{
+			yield return new BlockCloneBeforeColision(_driveParams);
+			yield return new BlockCloneIfGoingLeftAndElevatorIsOnRight(_driveParams);
+			yield return new BlockCloneIfGoingLeftAndExitIsOnRight(_driveParams);
+			yield return new BlockCloneIfGoingRightAndElevatorIsOnLeft(_driveParams);
+			yield return new BlockCloneIfGoingRightAndExitIsOnLeft(_driveParams);
+			yield return new Wait(_driveParams);
+		}
+	}
+
 	public class CloneMaster
 	{
 		private readonly DriveParams _driveParams;
 		private readonly int[] _blockedClonesPerFloor;
+		private readonly IEnumerable<TurnDecisionBase> _turnDecisions;
 
-		public CloneMaster(DriveParams driveParams)
+		public CloneMaster(DriveParams driveParams, TurnDecisionsFactory turnDecisionsFactory)
 		{
 			_driveParams = driveParams;
 			_blockedClonesPerFloor = new int[driveParams.FloorCount];
+			_turnDecisions = turnDecisionsFactory.Create();
 		}
 
 		public TurnDecision Decide(TurnParams turnParams)
 		{
-			if (turnParams.IsLeftColision() || turnParams.IsRightColision(_driveParams.DriveWidth))
-			{
-				_blockedClonesPerFloor[turnParams.CloneFloor]++;
-				return TurnDecision.BLOCK;
-			}
-
-			var currentFloorElevator = _driveParams.Elevators.FirstOrDefault(e => e.Floor == turnParams.CloneFloor);
-			var previousFloorElevator = _driveParams.Elevators.FirstOrDefault(e => e.Floor == turnParams.CloneFloor - 1);
-			if (turnParams.ClonePosition > currentFloorElevator?.Position
-				&& turnParams.Direction == Direction.RIGHT
-				&& turnParams.CloneFloor == currentFloorElevator?.Floor
-				&& previousFloorElevator?.Position + 1 == turnParams.ClonePosition
-				&& _blockedClonesPerFloor[turnParams.CloneFloor] == 0)
-			{
-				_blockedClonesPerFloor[turnParams.CloneFloor]++;
-				return TurnDecision.BLOCK;
-			}
-
-			if (turnParams.ClonePosition > _driveParams.ExitPosition
-				&& turnParams.Direction == Direction.RIGHT
-				&& turnParams.CloneFloor == _driveParams.ExitFloor
-				&& previousFloorElevator?.Position + 1 == turnParams.ClonePosition
-				&& _blockedClonesPerFloor[turnParams.CloneFloor] == 0)
-			{
-				_blockedClonesPerFloor[turnParams.CloneFloor]++;
-				return TurnDecision.BLOCK;
-			}
-
-			if (turnParams.ClonePosition < currentFloorElevator?.Position
-				&& turnParams.Direction == Direction.LEFT
-				&& turnParams.CloneFloor == currentFloorElevator?.Floor
-				&& previousFloorElevator?.Position - 1 == turnParams.ClonePosition
-				&& _blockedClonesPerFloor[turnParams.CloneFloor] == 0)
-			{
-				_blockedClonesPerFloor[turnParams.CloneFloor]++;
-				return TurnDecision.BLOCK;
-			}
-
-			if (turnParams.ClonePosition < _driveParams.ExitPosition
-				&& turnParams.Direction == Direction.LEFT
-				&& turnParams.CloneFloor == _driveParams.ExitFloor
-				&& previousFloorElevator?.Position - 1 == turnParams.ClonePosition
-				&& _blockedClonesPerFloor[turnParams.CloneFloor] == 0)
-			{
-				_blockedClonesPerFloor[turnParams.CloneFloor]++;
-				return TurnDecision.BLOCK;
-			}
-
-			return TurnDecision.WAIT;
+			return _turnDecisions
+				.First(td => td.CanDecide(turnParams, _blockedClonesPerFloor))
+				.Decide(turnParams, _blockedClonesPerFloor);
 		}
 	}
 
