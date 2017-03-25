49b0632b917e7ba1cfc410fac5efe3356b676e58
Alin Dicu <alinddicu@yahoo.com>
2017-03-25 10:44:07 +0100

    added and used reference to codingame.common.build.submit.file

--- codingame-dont-panic/codingame.dont.panic.build/Program.cs
+++ codingame-dont-panic/codingame.dont.panic.build/Program.cs
@@ -1,32 +1,14 @@
 ﻿namespace codingame.dont.panic.build
 {
-	using System.IO;
-	using System.Linq;
-	using System.Text;
+	using common.build.submit.file;
 
 	public class Program
 	{
 		public static void Main(string[] args)
 		{
-			const string folderPath = "../../../codingame.dont.panic/";
-			var contents = new StringBuilder();
-			AppendFilesContents(folderPath, contents);
-
-			File.WriteAllText("../../codingame.dont.panic.txt", contents.ToString());
-		}
-
-		private static void AppendFilesContents(string root, StringBuilder contents)
-		{
-			foreach (var file in Directory.EnumerateFiles(root, "*.cs").Where(f => !f.EndsWith("AssemblyInfo.cs")))
-			{
-				contents.AppendLine(File.ReadAllText(file));
-				contents.AppendLine();
-			}
-
-			foreach (var d in Directory.GetDirectories(root))
-			{
-				AppendFilesContents(d, contents);
-			}
+			const string sourceFolderPath = "../../../codingame.dont.panic/";
+			const string targetFilePath = "../../codingame.dont.panic.txt";
+			new BuildSubmitFile().Build(sourceFolderPath, targetFilePath);
 		}
 	}
 }
