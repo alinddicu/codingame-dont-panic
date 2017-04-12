ed3db5fe165086ae7cfdd471bc3c61940eb7f03e
Alin Dicu <alinddicu@yahoo.com>
2017-04-12 17:38:43 +0200

    fix build and unit tests

--- codingame-dont-panic/codingame.dont.panic.test/TurnParamsTest.cs
+++ codingame-dont-panic/codingame.dont.panic.test/TurnParamsTest.cs
@@ -6,17 +6,7 @@
 	[TestClass]
 	public class TurnParamsTest
 	{
-		[TestMethod]
-		public void WhenCreateTurnParamsThenValuesOfTurnParamsAreCorrect()
-		{
-			var turnParams = new TurnParams("8 7 RIGHT", null);
-
-			Check.That(turnParams.CloneFloor).IsEqualTo(8);
-			Check.That(turnParams.ClonePosition).IsEqualTo(7);
-			Check.That(turnParams.CloneDirection).IsEqualTo(Direction.RIGHT);
-		}
-
-		private static DriveParams GetDriveParams(
+		private static DriveParams CreateDriveParams(
 				int floorCount,
 				int driveWidth,
 				int exitFloor,
@@ -37,7 +27,7 @@
 		[TestMethod]
 		public void GivenTurnParams00LeftWhenIsLeftCollisionThenTrue()
 		{
-			var driveParams = GetDriveParams(2, 3, 1, 3, 0);
+			var driveParams = CreateDriveParams(2, 3, 1, 3, 0);
 			var turnParams = new TurnParams("0 0 LEFT", driveParams);
 
 			Check.That(turnParams.IsColision()).IsTrue();
@@ -46,7 +36,7 @@
 		[TestMethod]
 		public void GivenTurnParams00RightWhenIsLeftCollisionThenFalse()
 		{
-			var driveParams = GetDriveParams(2, 3, 1, 3, 0);
+			var driveParams = CreateDriveParams(2, 3, 1, 3, 0);
 			var turnParams = new TurnParams("0 0 RIGHT", driveParams);
 
 			Check.That(turnParams.IsColision()).IsFalse();
@@ -55,7 +45,7 @@
 		[TestMethod]
 		public void GivenTurnParams02RightAndDriveWidthOf3WhenIsLeftCollisionThenTrue()
 		{
-			var driveParams = GetDriveParams(2, 3, 1, 3, 0);
+			var driveParams = CreateDriveParams(2, 3, 1, 3, 0);
 			var turnParams = new TurnParams("0 2 RIGHT", driveParams);
 
 			Check.That(turnParams.IsColision()).IsTrue();
@@ -64,71 +54,71 @@
 		[TestMethod]
 		public void GivenTurnParams01RightAndDriveWidthOf3WhenIsLeftCollisionThenFalse()
 		{
-			var driveParams = GetDriveParams(2, 3, 1, 3, 0);
+			var driveParams = CreateDriveParams(2, 3, 1, 3, 0);
 			var turnParams = new TurnParams("0 1 RIGHT", driveParams);
 
 			Check.That(turnParams.IsColision()).IsFalse();
 		}
 
 		[TestMethod]
-		public void GivenElevatorAtLeftAndCloneGoingRightWhenShouldCloneReverseThenReturnTrue()
+		public void GivenElevatorAtLeftAndCloneGoingRightWhenShouldNextCloneReverserseThenReturnTrue()
 		{
-			var driveParams = GetDriveParams(3, 4, 3, 3, 2, new Elevator("1 0"), new Elevator("3 0"));
+			var driveParams = CreateDriveParams(3, 4, 3, 3, 2, new Elevator("1 0"), new Elevator("3 0"));
 			var turnParams = new TurnParams("1 3 RIGHT", driveParams);
 
-			Check.That(turnParams.ShouldCloneReverse(Direction.RIGHT)).IsTrue();
+			Check.That(turnParams.ShouldNextCloneReverse(Direction.RIGHT)).IsTrue();
 		}
 
 		[TestMethod]
-		public void GivenElevatorAtLeftAndCloneGoingRightAndReferenceDirectionLeftWhenShouldCloneReverseThenReturnFalse()
+		public void GivenElevatorAtLeftAndCloneGoingRightAndReferenceDirectionLeftWhenShouldNextCloneReverserseThenReturnFalse()
 		{
-			var driveParams = GetDriveParams(3, 4, 3, 3, 2, new Elevator("1 0"), new Elevator("3 0"));
+			var driveParams = CreateDriveParams(3, 4, 3, 3, 2, new Elevator("1 0"), new Elevator("3 0"));
 			var turnParams = new TurnParams("1 3 RIGHT", driveParams);
 
-			Check.That(turnParams.ShouldCloneReverse(Direction.LEFT)).IsFalse();
+			Check.That(turnParams.ShouldNextCloneReverse(Direction.LEFT)).IsFalse();
 		}
 
 		[TestMethod]
-		public void GivenElevatorAtLeftAndCloneGoingLeftWhenShouldCloneReverseThenReturnFalse()
+		public void GivenElevatorAtLeftAndCloneGoingLeftWhenShouldNextCloneReverserseThenReturnFalse()
 		{
-			var driveParams = GetDriveParams(3, 4, 3, 3, 2, new Elevator("1 0"), new Elevator("3 0"));
+			var driveParams = CreateDriveParams(3, 4, 3, 3, 2, new Elevator("1 0"), new Elevator("3 0"));
 			var turnParams = new TurnParams("1 3 LEFT", driveParams);
 
-			Check.That(turnParams.ShouldCloneReverse(Direction.LEFT)).IsFalse();
+			Check.That(turnParams.ShouldNextCloneReverse(Direction.LEFT)).IsFalse();
 		}
 
 		[TestMethod]
-		public void GivenExitAtLeftAndCloneGoingRightWhenShouldCloneReverseThenReturnTrue()
+		public void GivenExitAtLeftAndCloneGoingRightWhenShouldNextCloneReverserseThenReturnTrue()
 		{
-			var driveParams = GetDriveParams(3, 4, 3, 0, 2, new Elevator("1 0"), new Elevator("3 0"));
+			var driveParams = CreateDriveParams(3, 4, 3, 0, 2, new Elevator("1 0"), new Elevator("3 0"));
 			var turnParams = new TurnParams("3 1 RIGHT", driveParams);
 
-			Check.That(turnParams.ShouldCloneReverse(Direction.RIGHT)).IsTrue();
+			Check.That(turnParams.ShouldNextCloneReverse(Direction.RIGHT)).IsTrue();
 		}
 
 		[TestMethod]
-		public void GivenExitAtLeftAndCloneGoingLeftWhenShouldCloneReverseThenReturnFalse()
+		public void GivenExitAtLeftAndCloneGoingLeftWhenShouldNextCloneReverserseThenReturnFalse()
 		{
-			var driveParams = GetDriveParams(3, 4, 3, 0, 2, new Elevator("1 0"), new Elevator("3 0"));
+			var driveParams = CreateDriveParams(3, 4, 3, 0, 2, new Elevator("1 0"), new Elevator("3 0"));
 			var turnParams = new TurnParams("3 1 LEFT", driveParams);
 
-			Check.That(turnParams.ShouldCloneReverse(Direction.LEFT)).IsFalse();
+			Check.That(turnParams.ShouldNextCloneReverse(Direction.LEFT)).IsFalse();
 		}
 
 		[TestMethod]
-		public void Given2ElevatorPerFloorWhenShouldCloneReverseThenReturnTrue()
+		public void Given2ElevatorPerFloorWhenShouldNextCloneReverserseThenReturnTrue()
 		{
 			var elevators = new[] { new Elevator("0 4"), new Elevator("1 0"), new Elevator("1 5") };
-			var driveParams = GetDriveParams(3, 9, 2, 8, 3, elevators);
+			var driveParams = CreateDriveParams(3, 9, 2, 8, 3, elevators);
 			var turnParams = new TurnParams("1 3 LEFT", driveParams);
 
-			Check.That(turnParams.ShouldCloneReverse(Direction.LEFT)).IsTrue();
+			Check.That(turnParams.ShouldNextCloneReverse(Direction.LEFT)).IsTrue();
 		}
 
 		[TestMethod]
 		public void GivenGroundFloorAndGoingLeftWhenIsCloneNearPreviousElevatorTheFalse()
 		{
-			var driveParams = GetDriveParams(1, 4, 1, 3, 1, new Elevator("0 0"));
+			var driveParams = CreateDriveParams(1, 4, 1, 3, 1, new Elevator("0 0"));
 			var turnParams = new TurnParams("0 2 LEFT", driveParams);
 
 			Check.That(turnParams.IsCloneNearPreviousElevator(Direction.LEFT)).IsFalse();
@@ -137,7 +127,7 @@
 		[TestMethod]
 		public void GivenGroundFloorAndGoingRightWhenIsCloneNearPreviousElevatorTheFalse()
 		{
-			var driveParams = GetDriveParams(1, 4, 1, 3, 1, new Elevator("0 0"));
+			var driveParams = CreateDriveParams(1, 4, 1, 3, 1, new Elevator("0 0"));
 			var turnParams = new TurnParams("0 1 RIGHT", driveParams);
 
 			Check.That(turnParams.IsCloneNearPreviousElevator(Direction.RIGHT)).IsFalse();
@@ -146,7 +136,7 @@
 		[TestMethod]
 		public void Given1StFloorAndGoingRightWhenIsCloneNearPreviousElevatorTheTrue()
 		{
-			var driveParams = GetDriveParams(2, 4, 1, 3, 1, new Elevator("0 0"));
+			var driveParams = CreateDriveParams(2, 4, 1, 3, 1, new Elevator("0 0"));
 			var turnParams = new TurnParams("1 1 RIGHT", driveParams);
 
 			Check.That(turnParams.IsCloneNearPreviousElevator(Direction.RIGHT)).IsTrue();
@@ -155,7 +145,7 @@
 		[TestMethod]
 		public void Given1StFloorAndGoingLeftWhenIsCloneNearPreviousElevatorTheTrue()
 		{
-			var driveParams = GetDriveParams(2, 4, 1, 0, 1, new Elevator("0 2"));
+			var driveParams = CreateDriveParams(2, 4, 1, 0, 1, new Elevator("0 2"));
 			var turnParams = new TurnParams("1 1 LEFT", driveParams);
 
 			Check.That(turnParams.IsCloneNearPreviousElevator(Direction.LEFT)).IsTrue();
@@ -164,7 +154,7 @@
 		[TestMethod]
 		public void Given1StFloorAndGoingLeftWhenNotIsCloneNearPreviousElevatorTheFalse()
 		{
-			var driveParams = GetDriveParams(2, 4, 1, 0, 1, new Elevator("0 2"));
+			var driveParams = CreateDriveParams(2, 4, 1, 0, 1, new Elevator("0 2"));
 			var turnParams = new TurnParams("1 2 LEFT", driveParams);
 
 			Check.That(turnParams.IsCloneNearPreviousElevator(Direction.LEFT)).IsFalse();
